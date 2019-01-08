d079dbce3ad8bcd565e0a05dbdab07bbfc89a63c
Ruben Taelman <ruben.taelman@ugent.be>
2019-01-08 15:12:37 +0100

    Add support for mutation expressions in SparqlHandler

--- src/SparqlHandler.js
+++ src/SparqlHandler.js
@@ -1,27 +1,54 @@
 /**
- * Expresses a path as a SPARQL query.
+ * Expresses a path or mutation as a SPARQL query.
  *
  * Requires:
- * - a pathExpression property on the path proxy
+ * - a mutationExpression or pathExpression property on the path proxy
  */
 export default class SparqlHandler {
   async execute(path, proxy) {
+    // First check if we have a mutation expression
+    const mutationExpression = await proxy.mutationExpression;
+    if (Array.isArray(mutationExpression))
+      return this.executeMutationExpression(path, proxy, mutationExpression);
+
+    // Otherwise, fallback to checking for a path expression
     const pathExpression = await proxy.pathExpression;
     if (!Array.isArray(pathExpression))
       throw new Error(`${path} has no pathExpression property`);
+    return this.executePathExpression(path, proxy, pathExpression);
+  }
 
+  executePathExpression(path, proxy, pathExpression) {
     // Require at least a subject and a link
     if (pathExpression.length < 2)
       throw new Error(`${path} should at least contain a subject and a predicate`);
-    const root = pathExpression.shift();
 
     // Determine the query variable name
     const queryVar = path.property.match(/[a-z0-9]*$/i)[0] || 'result';
 
     // Build basic graph pattern
-    const last = pathExpression.length - 1;
+    const clauses = this.expressionToTriplePatterns(pathExpression, queryVar);
+
+    // Embed the basic graph pattern into a SPARQL query
+    const joinedClauses = clauses.join('\n  ');
+    return `SELECT ?${queryVar} WHERE {\n  ${joinedClauses}\n}`;
+  }
+
+  executeMutationExpression(path, proxy, mutationExpressions) {
+    // Require at least one mutation query
+    if (!mutationExpressions.length)
+      throw new Error(`${path} should at least have one mutation expression`);
+
+    return mutationExpressions
+      .map(mutationExpression => this.mutationExpressionToQuery(mutationExpression))
+      .join('\n;\n');
+  }
+
+  expressionToTriplePatterns(pathExpression, queryVar) {
+    const root = pathExpression[0];
+    const last = pathExpression.length - 2;
     let object = `<${root.subject}>`;
-    const clauses = pathExpression.map((segment, index) => {
+    return pathExpression.slice(1).map((segment, index) => {
       // Obtain triple pattern components
       const subject = object;
       const { predicate } = segment;
@@ -29,9 +56,43 @@ export default class SparqlHandler {
       // Generate triple pattern
       return `${subject} <${predicate}> ${object}.`;
     });
+  }
 
-    // Embed the basic graph pattern into a SPARQL query
-    const joinedClauses = clauses.join('\n  ');
-    return `SELECT ?${queryVar} WHERE {\n  ${joinedClauses}\n}`;
+  mutationExpressionToQuery({ mutationType, domainExpression, predicate, rangeExpression }) {
+    // Determine the patterns that should appear in the WHERE clause
+    const { queryVar: domainQueryVar, clause: domainClause } = this.getQueryVarAndClauses(domainExpression);
+    const { queryVar: rangeQueryVar, clause: rangeClause } = this.getQueryVarAndClauses(rangeExpression);
+    const clauses = [];
+    if (domainClause)
+      clauses.push(domainClause);
+    if (rangeClause)
+      clauses.push(rangeClause);
+
+    // Determine the insert pattern
+    const insertPattern = `${domainQueryVar} <${predicate}> ${rangeQueryVar}`;
+
+    // If we don't have any WHERE clauses, we just insert raw data
+    if (!clauses.length)
+      return `${mutationType} DATA {\n  ${insertPattern}\n}`;
+
+    // Otherwise, return an INSERT ... WHERE ... query
+    return `${mutationType} {\n  ${insertPattern}\n} WHERE {\n  ${clauses.join('\n\n  ')}\n}`;
+  }
+
+  getQueryVarAndClauses(expression) {
+    const lastSegment = expression[expression.length - 1];
+
+    if (expression.length === 1) {
+      return {
+        queryVar: lastSegment.subject[0] === '"' ? lastSegment.subject : `<${lastSegment.subject}>`,
+        clauses: [],
+      };
+    }
+
+    const queryVar = lastSegment.predicate.match(/[a-z0-9]*$/i)[0] || 'result';
+    return {
+      queryVar: `?${queryVar}`,
+      clause: this.expressionToTriplePatterns(expression, queryVar).join('\n  '),
+    };
   }
 }
--- test/unit/SparqlHandler-test.js
+++ test/unit/SparqlHandler-test.js
@@ -13,56 +13,269 @@ describe('a SparqlHandler instance', () => {
       .toThrow(new Error('path has no pathExpression property'));
   });
 
-  it('errors with a path of length 0', async () => {
-    const pathExpression = [
-      { subject: 'https://example.org/#me' },
-    ];
-    const path = { toString: () => 'path' };
+  describe('with a pathExpression', () => {
+    it('errors with a path of length 0', async () => {
+      const pathExpression = [
+        { subject: 'https://example.org/#me' },
+      ];
+      const path = { toString: () => 'path' };
 
-    await expect(handler.execute(path, { pathExpression })).rejects
-      .toThrow(new Error('path should at least contain a subject and a predicate'));
-  });
+      await expect(handler.execute(path, { pathExpression })).rejects
+        .toThrow(new Error('path should at least contain a subject and a predicate'));
+    });
 
-  it('resolves a path of length 1', async () => {
-    const pathExpression = [
-      { subject: 'https://example.org/#me' },
-      { predicate: 'https://ex.org/p1' },
-    ];
-    const path = { property: 'p1' };
+    it('resolves a path of length 1', async () => {
+      const pathExpression = [
+        { subject: 'https://example.org/#me' },
+        { predicate: 'https://ex.org/p1' },
+      ];
+      const path = { property: 'p1' };
 
-    expect(await handler.execute(path, { pathExpression })).toEqual(deindent(`
+      expect(await handler.execute(path, { pathExpression })).toEqual(deindent(`
       SELECT ?p1 WHERE {
         <https://example.org/#me> <https://ex.org/p1> ?p1.
       }`));
-  });
+    });
 
-  it('resolves a path of length 3', async () => {
-    const pathExpression = [
-      { subject: 'https://example.org/#me' },
-      { predicate: 'https://ex.org/p1' },
-      { predicate: 'https://ex.org/p2' },
-      { predicate: 'https://ex.org/p3' },
-    ];
-    const path = { property: 'p3' };
+    it('resolves a path of length 3', async () => {
+      const pathExpression = [
+        { subject: 'https://example.org/#me' },
+        { predicate: 'https://ex.org/p1' },
+        { predicate: 'https://ex.org/p2' },
+        { predicate: 'https://ex.org/p3' },
+      ];
+      const path = { property: 'p3' };
 
-    expect(await handler.execute(path, { pathExpression })).toEqual(deindent(`
+      expect(await handler.execute(path, { pathExpression })).toEqual(deindent(`
       SELECT ?p3 WHERE {
         <https://example.org/#me> <https://ex.org/p1> ?v0.
         ?v0 <https://ex.org/p2> ?v1.
         ?v1 <https://ex.org/p3> ?p3.
       }`));
-  });
+    });
 
-  it('resolves a path with an property name ending in a non-word', async () => {
-    const pathExpression = [
-      { subject: 'https://example.org/#me' },
-      { predicate: 'https://ex.org/p1' },
-    ];
-    const path = { property: '/x/' };
+    it('resolves a path with an property name ending in a non-word', async () => {
+      const pathExpression = [
+        { subject: 'https://example.org/#me' },
+        { predicate: 'https://ex.org/p1' },
+      ];
+      const path = { property: '/x/' };
 
-    expect(await handler.execute(path, { pathExpression })).toEqual(deindent(`
+      expect(await handler.execute(path, { pathExpression })).toEqual(deindent(`
       SELECT ?result WHERE {
         <https://example.org/#me> <https://ex.org/p1> ?result.
       }`));
+    });
+  });
+
+  describe('with a mutationExpression', () => {
+    it('errors with 0 expressions', async () => {
+      const mutationExpression = [];
+      const path = { toString: () => 'path' };
+
+      await expect(handler.execute(path, { mutationExpression })).rejects
+        .toThrow(new Error('path should at least have one mutation expression'));
+    });
+
+    describe('with one INSERT expression', () => {
+      it('resolves with domain of length 0 and range of length of 0', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [{ subject: 'https://example.org/#D0' }],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: 'https://example.org/#R0' }],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT DATA {
+        <https://example.org/#D0> <https://example.org/p> <https://example.org/#R0>
+      }`));
+      });
+
+      it('resolves with domain of length 0 and range of length of 0 with literal', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [{ subject: 'https://example.org/#D0' }],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: '"Ruben"' }],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT DATA {
+        <https://example.org/#D0> <https://example.org/p> "Ruben"
+      }`));
+      });
+
+      it('resolves with domain of length 2 and range of length of 0', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [
+              { subject: 'https://example.org/#D0' },
+              { predicate: 'https://example.org/#Dp1' },
+              { predicate: 'https://example.org/#Dp2' },
+            ],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: 'https://example.org/#R0' }],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT {
+        ?Dp2 <https://example.org/p> <https://example.org/#R0>
+      } WHERE {
+        <https://example.org/#D0> <https://example.org/#Dp1> ?v0.
+        ?v0 <https://example.org/#Dp2> ?Dp2.
+      }`));
+      });
+
+      it('resolves with domain of length 1 with exotic predicate and range of length of 0', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [
+              { subject: 'https://example.org/#D0' },
+              { predicate: 'https://example.org/#' },
+            ],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: 'https://example.org/#R0' }],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT {
+        ?result <https://example.org/p> <https://example.org/#R0>
+      } WHERE {
+        <https://example.org/#D0> <https://example.org/#> ?result.
+      }`));
+      });
+
+      it('resolves with domain of length 0 and range of length of 2', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [{ subject: 'https://example.org/#D0' }],
+            predicate: 'https://example.org/p',
+            rangeExpression: [
+              { subject: 'https://example.org/#R0' },
+              { predicate: 'https://example.org/#Rp1' },
+              { predicate: 'https://example.org/#Rp2' },
+            ],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT {
+        <https://example.org/#D0> <https://example.org/p> ?Rp2
+      } WHERE {
+        <https://example.org/#R0> <https://example.org/#Rp1> ?v0.
+        ?v0 <https://example.org/#Rp2> ?Rp2.
+      }`));
+      });
+
+      it('resolves with domain of length 2 and range of length of 2', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [
+              { subject: 'https://example.org/#D0' },
+              { predicate: 'https://example.org/#Dp1' },
+              { predicate: 'https://example.org/#Dp2' },
+            ],
+            predicate: 'https://example.org/p',
+            rangeExpression: [
+              { subject: 'https://example.org/#R0' },
+              { predicate: 'https://example.org/#Rp1' },
+              { predicate: 'https://example.org/#Rp2' },
+            ],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT {
+        ?Dp2 <https://example.org/p> ?Rp2
+      } WHERE {
+        <https://example.org/#D0> <https://example.org/#Dp1> ?v0.
+        ?v0 <https://example.org/#Dp2> ?Dp2.
+      
+        <https://example.org/#R0> <https://example.org/#Rp1> ?v0.
+        ?v0 <https://example.org/#Rp2> ?Rp2.
+      }`));
+      });
+    });
+
+    describe('with one DELETE expression', () => {
+      it('resolves with domain of length 0 and range of length of 0', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'DELETE',
+            domainExpression: [{ subject: 'https://example.org/#D0' }],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: 'https://example.org/#R0' }],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      DELETE DATA {
+        <https://example.org/#D0> <https://example.org/p> <https://example.org/#R0>
+      }`));
+      });
+    });
+
+    describe('with multiple expressions', () => {
+      it('resolves with queries separated by semicolons', async () => {
+        const mutationExpression = [
+          {
+            mutationType: 'INSERT',
+            domainExpression: [
+              { subject: 'https://example.org/#D0' },
+              { predicate: 'https://example.org/#Dp1' },
+              { predicate: 'https://example.org/#Dp2' },
+            ],
+            predicate: 'https://example.org/p',
+            rangeExpression: [
+              { subject: 'https://example.org/#R0' },
+              { predicate: 'https://example.org/#Rp1' },
+              { predicate: 'https://example.org/#Rp2' },
+            ],
+          },
+          {
+            mutationType: 'DELETE',
+            domainExpression: [{ subject: 'https://example.org/#D0' }],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: 'https://example.org/#R0' }],
+          },
+          {
+            mutationType: 'INSERT',
+            domainExpression: [{ subject: 'https://example.org/#D0' }],
+            predicate: 'https://example.org/p',
+            rangeExpression: [{ subject: 'https://example.org/#R0' }],
+          },
+        ];
+
+        await expect(await handler.execute({}, { mutationExpression })).toEqual(deindent(`
+      INSERT {
+        ?Dp2 <https://example.org/p> ?Rp2
+      } WHERE {
+        <https://example.org/#D0> <https://example.org/#Dp1> ?v0.
+        ?v0 <https://example.org/#Dp2> ?Dp2.
+      
+        <https://example.org/#R0> <https://example.org/#Rp1> ?v0.
+        ?v0 <https://example.org/#Rp2> ?Rp2.
+      }
+      ;
+      DELETE DATA {
+        <https://example.org/#D0> <https://example.org/p> <https://example.org/#R0>
+      }
+      ;
+      INSERT DATA {
+        <https://example.org/#D0> <https://example.org/p> <https://example.org/#R0>
+      }`));
+      });
+    });
   });
 });
