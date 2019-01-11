81f07c7012360d752217eaf31bc12286cbee3ed6
Ruben Taelman <ruben.taelman@ugent.be>
2019-01-11 09:44:36 +0100

    Document chaining in MutationExpressionsHandler

--- src/MutationExpressionsHandler.js
+++ src/MutationExpressionsHandler.js
@@ -1,5 +1,6 @@
 /**
  * Traverses a path to collect mutationExpressions into an expression.
+ * This is needed because mutations can be chained.
  *
  * Requires:
  * - a mutationExpressions property on the path proxy
