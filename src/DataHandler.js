3892183f7b197f44cbc53b3098096cdc1a0e748d
Ruben Verborgh <ruben@verborgh.org>
2019-01-31 21:32:25 +0100

    Improve DataHandler documentation.

--- src/DataHandler.js
+++ src/DataHandler.js
@@ -1,53 +1,55 @@
 /**
  * Resolves to the given item in the path data.
+ * For example, new DataHandler({}, 'foo', 'bar')
+ * will return pathData.foo.bar.
  *
- * This resolution can optionally be async,
+ * Resolution can optionally be async,
  * and/or be behind a function call.
  */
 export default class DataHandler {
-  constructor(options, ...pathSegments) {
-    this._async = options.async;
-    this._function = options.function;
-    this._pathSegments = pathSegments;
+  constructor(options, ...dataProperties) {
+    this._isAsync = options.async;
+    this._isFunction = options.function;
+    this._dataProperties = dataProperties;
   }
 
-  static sync(...pathSegments) {
-    return new DataHandler({ async: false }, ...pathSegments);
+  static sync(...dataProperties) {
+    return new DataHandler({ async: false }, ...dataProperties);
   }
 
-  static syncFunction(...pathSegments) {
-    return new DataHandler({ async: false, function: true }, ...pathSegments);
+  static syncFunction(...dataProperties) {
+    return new DataHandler({ async: false, function: true }, ...dataProperties);
   }
 
-  static async(...pathSegments) {
-    return new DataHandler({ async: true }, ...pathSegments);
+  static async(...dataProperties) {
+    return new DataHandler({ async: true }, ...dataProperties);
   }
 
-  static asyncFunction(...pathSegments) {
-    return new DataHandler({ async: true, function: true }, ...pathSegments);
+  static asyncFunction(...dataProperties) {
+    return new DataHandler({ async: true, function: true }, ...dataProperties);
   }
 
   /**
    * Resolve the data path.
    */
   handle(pathData) {
-    return !this._function ?
+    return !this._isFunction ?
       this._resolveDataPath(pathData) :
       () => this._resolveDataPath(pathData);
   }
 
   _resolveDataPath(data) {
     // Resolve synchronous property access
-    if (!this._async) {
-      for (const pathSegment of this._pathSegments)
-        data = data && data[pathSegment];
+    if (!this._isAsync) {
+      for (const property of this._dataProperties)
+        data = data && data[property];
       return data;
     }
 
     // Resolve asynchronous property access
     return new Promise(async resolve => {
-      for (const pathSegment of this._pathSegments)
-        data = data && await data[pathSegment];
+      for (const property of this._dataProperties)
+        data = data && await data[property];
       resolve(data);
     });
   }
