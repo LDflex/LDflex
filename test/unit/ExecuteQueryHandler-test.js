77c7058d8f739c1a78c3284ca23128cf9eedaa13
Ruben Verborgh <ruben@verborgh.org>
2019-11-11 21:13:24 +0100

    Remove multi-element mode reference.

--- test/unit/ExecuteQueryHandler-test.js
+++ test/unit/ExecuteQueryHandler-test.js
@@ -2,31 +2,29 @@ import ExecuteQueryHandler from '../../src/ExecuteQueryHandler';
 import { literal } from '@rdfjs/data-model';
 
 describe('a ExecuteQueryHandler instance', () => {
-  describe('in multi-element mode', () => {
-    let handler;
-    beforeAll(() => handler = new ExecuteQueryHandler());
+  let handler;
+  beforeAll(() => handler = new ExecuteQueryHandler());
 
-    it('errors when a path defines no query engine', async () => {
-      const path = { settings: {}, toString: () => 'path' };
-      const iterable = handler.handle(path, {});
-      const iterator = iterable[Symbol.asyncIterator]();
-      await expect(iterator.next()).rejects
-        .toThrow(new Error('path has no queryEngine setting'));
-    });
+  it('errors when a path defines no query engine', async () => {
+    const path = { settings: {}, toString: () => 'path' };
+    const iterable = handler.handle(path, {});
+    const iterator = iterable[Symbol.asyncIterator]();
+    await expect(iterator.next()).rejects
+      .toThrow(new Error('path has no queryEngine setting'));
+  });
 
-    it('errors when a path defines no sparql property', async () => {
-      const path = { settings: { queryEngine: {} }, toString: () => 'path' };
-      const iterable = handler.handle(path, {});
-      const iterator = iterable[Symbol.asyncIterator]();
-      await expect(iterator.next()).rejects
-        .toThrow(new Error('path has no sparql property'));
-    });
+  it('errors when a path defines no sparql property', async () => {
+    const path = { settings: { queryEngine: {} }, toString: () => 'path' };
+    const iterable = handler.handle(path, {});
+    const iterator = iterable[Symbol.asyncIterator]();
+    await expect(iterator.next()).rejects
+      .toThrow(new Error('path has no sparql property'));
+  });
 
-    it('errors with multi-variable results', async () => {
-      const bindings = new Map([['?a', literal('')], ['?b', literal('')]]);
-      const path = { extendPath: args => args };
-      await expect(() => handler.extractTerm(bindings, path))
-        .toThrow(new Error('Only single-variable queries are supported'));
-    });
+  it('errors with multi-variable results', async () => {
+    const bindings = new Map([['?a', literal('')], ['?b', literal('')]]);
+    const path = { extendPath: args => args };
+    await expect(() => handler.extractTerm(bindings, path))
+      .toThrow(new Error('Only single-variable queries are supported'));
   });
 });
